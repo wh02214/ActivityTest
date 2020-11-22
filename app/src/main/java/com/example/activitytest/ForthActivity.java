bbd1ecbfb5a79bf15945a7b773c8cc5926542ce3
wh02214 <331670419@qq.com>
2020-11-22 12:10:03 +0800

    1.0

--- app/src/main/java/com/example/activitytest/ForthActivity.java
+++ app/src/main/java/com/example/activitytest/ForthActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ForthActivity extends AppCompatActivity {
 
@@ -14,11 +15,18 @@ public class ForthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forth);
-        ImageButton imageButton=(ImageButton)findViewById(R.id.button_back);
+        ImageButton imageButton = (ImageButton) findViewById(R.id.button_back);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        Button button = (Button) findViewById(R.id.button_sign_up);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ForthActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
             }
         });
     }
