1e1c24f26e244de370e00283c02d46ff5d6847fb
wh02214 <331670419@qq.com>
2020-11-24 23:29:19 +0800

    1.0

--- app/src/main/java/com/example/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -14,19 +14,22 @@ import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private EditText accountEdit;
+    private EditText passwordEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        accountEdit = (EditText) findViewById(R.id.edit_account);
+        passwordEdit = (EditText) findViewById(R.id.edit_password);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
-        Button button = (Button) findViewById(R.id.button_2);
+        Button button = (Button) findViewById(R.id.sigh_up);
         ImageButton imageButton = (ImageButton) findViewById(R.id.imagebutton_sina);//新浪登录
         ImageButton imageButton1 = (ImageButton) findViewById(R.id.imagebutton_tencent);//腾讯登录
-        RadioButton radioButton = (RadioButton) findViewById(R.id.radiobutton1);        //Radiobutton
         button.setOnClickListener(new View.OnClickListener() {                    //注册
             @Override
             public void onClick(View v) {
@@ -35,12 +38,19 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        Button button1 = (Button) findViewById(R.id.button_4);
+        Button button1 = (Button) findViewById(R.id.sigh_in);
         button1.setOnClickListener(new View.OnClickListener() {               //登录
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
-                startActivity(intent);
+                //登录逻辑
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                if (account.equals("123456") && password.equals("123456")) {
+                    Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(SecondActivity.this, "账号或密码错误", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         imageButton.setOnClickListener(new View.OnClickListener() {           //新浪
@@ -53,12 +63,6 @@ public class SecondActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(SecondActivity.this, "腾讯微博登录", Toast.LENGTH_SHORT).show();
-            }
-        });
-        radioButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SecondActivity.this, "Agree", Toast.LENGTH_SHORT).show();
             }
         });
 
