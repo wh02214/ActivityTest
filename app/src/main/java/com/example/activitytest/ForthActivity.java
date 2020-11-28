f422d6d734c746e24287d4963fc74ee60582a061
wh02214 <331670419@qq.com>
2020-11-28 20:38:48 +0800

    1.0

--- app/src/main/java/com/example/activitytest/ForthActivity.java
+++ app/src/main/java/com/example/activitytest/ForthActivity.java
@@ -45,11 +45,19 @@ public class ForthActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String account1 = account.getText().toString();
                 String password1 = password.getText().toString();
+                String password_re = password0.getText().toString();
                 SharedPreferences.Editor editor = getSharedPreferences("usersmessage", MODE_PRIVATE).edit();
-                editor.putString("account", account1);
-                editor.putString("password", password1);
-                editor.apply();
-                Toast.makeText(ForthActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                if (!password_re.equals(password1)) {
+                    Toast.makeText(ForthActivity.this, "两次输入的密码不一致", Toast.LENGTH_SHORT).show();
+                } else if (password_re.isEmpty() || account1.isEmpty() || password1.isEmpty()) {
+                    Toast.makeText(ForthActivity.this, "还有未输入的空格", Toast.LENGTH_SHORT).show();
+                } else {
+                    editor.putString("account", account1);
+                    editor.putString("password", password1);
+                    editor.apply();
+                    Toast.makeText(ForthActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
             }
         });
 
